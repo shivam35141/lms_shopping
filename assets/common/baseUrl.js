730b0a9f8c60def8dc602c74f7b98ee9026ea6d8
Shivam Mehta <shivam35141@gmail.com>
2022-02-06 23:55:19 +0530

    - Initial commit

---
+++ assets/common/baseUrl.js
@@ -0,0 +1,11 @@
+import { Platform } from 'react-native'
+
+
+let baseURL = '';
+
+{Platform.OS == 'android'
+? baseURL = 'http://192.168.0.179:3000/api/v1/'
+: baseURL = 'http://localhost:3000/api/v1/'
+}
+
+export default baseURL;
